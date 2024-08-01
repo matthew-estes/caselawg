2731fdda61015a1e4f76d8d149a316bed7587ab2
cbobak671 <cbobak.671@gmail.com>
2024-08-01 13:26:12 -0700

    Create url patterns and view functions for CaseCreate, CaseUpdate, and CaseDelete operations"

--- caselawg/main_app/urls.py
+++ caselawg/main_app/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path("about/", views.about, name="about"),
+    path('case/create', views.CaseCreate.as_view(), name='case-create'),
+    path('case/<int:pk>/update', views.CaseUpdate.as_view(), name='case-update'),
+    path('case/<int:pk>/delete', views.CaseDelete.as_view(), name='case-delete'),
 ]
--- caselawg/main_app/views.py
+++ caselawg/main_app/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Case
+
+class CaseCreate(CreateView):
+    model = Case
+    fields = '__all__'
+
+class CaseUpdate(UpdateView):
+    model = Case
+    fields = ['attorney', 'description', 'case_status', 'case_stage']
+
+class CaseDelete(DeleteView):
+    model = Case
+    success_url = '/cases/'
 
 def home(request):
     return HttpResponse('hello world')
 
 def about(request):
     return HttpResponse("<h1>Caselawg</h1>")
+
