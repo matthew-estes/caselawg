0dced1bb3c113da5699f674fda9ae644b7dd99fc
matthew-estes <matthew.estes19@gmail.com>
2024-08-01 16:55:36 -0400

    add URL pattern for the case_list view

--- caselawg/main_app/urls.py
+++ caselawg/main_app/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path("about/", views.about, name="about"),
+    path('cases/', views.case_list, name='case_list'),
 ]
