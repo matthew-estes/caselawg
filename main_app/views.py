7c9ba76373ddb54f7fc8a140d51c545f720e8d2f
cbobak671 <cbobak.671@gmail.com>
2024-08-02 11:43:54 -0700

    Create delete warning confirmation html

--- main_app/views.py
+++ main_app/views.py
@@ -15,8 +15,6 @@ def case_index(request):
 def case_detail(request, case_id):
     case = Case.objects.get(id=case_id)
     return render(request, "cases/case_detail.html", {'case': case})
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Case
 
 class CaseCreate(CreateView):
     model = Case
